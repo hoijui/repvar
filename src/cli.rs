ee2d66dc5b4b450a94b2e1e195ec23bd151a4b77
Robin Vobruba <hoijui.quaero@gmail.com>
2022-12-27 11:36:04 +0100

    Updates clap to 4.0

--- src/cli.rs
+++ src/cli.rs
@@ -2,7 +2,7 @@
 //
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-use clap::{app_from_crate, App, Arg, ValueHint};
+use clap::{command, Arg, ArgAction, Command, ValueHint};
 
 pub const A_S_INPUT: char = 'i';
 pub const A_L_INPUT: &str = "input";
@@ -21,8 +21,8 @@ pub const A_L_LIST: &str = "list";
 pub const A_S_FAIL_ON_MISSING_VALUES: char = 'f';
 pub const A_L_FAIL_ON_MISSING_VALUES: &str = "fail-on-missing-values";
 
-pub fn args_matcher() -> App<'static> {
-    app_from_crate!()
+pub fn args_matcher() -> Command {
+    command!()
         .about(
             "Given some text as input, \
             replaces variables of the type `${KEY}` with a respective value.",
@@ -30,31 +30,35 @@ pub fn args_matcher() -> App<'static> {
         .arg(
             Arg::new(A_L_INPUT)
                 .help("the input text file to use; '-' for stdin")
-                .takes_value(true)
+                .num_args(1)
                 .short(A_S_INPUT)
                 .long(A_L_INPUT)
-                .multiple_occurrences(false)
-                .default_value("-")
-                .required(false),
+                .action(ArgAction::Set)
+                .value_hint(ValueHint::FilePath)
+                .value_name("FILE")
+                .default_value("-"),
         )
         .arg(
             Arg::new(A_L_OUTPUT)
                 .help("the output text file to use; '-' for stdout")
-                .takes_value(true)
+                .num_args(1)
                 .short(A_S_OUTPUT)
                 .long(A_L_OUTPUT)
-                .multiple_occurrences(false)
-                .default_value("-")
-                .required(false),
+                .action(ArgAction::Set)
+                .value_hint(ValueHint::FilePath)
+                .value_name("FILE")
+                .default_value("-"),
         )
         .arg(
             Arg::new(A_L_VARIABLE)
                 .help("a variable key-value pair to be used for substitution in the text")
-                .takes_value(true)
+                .num_args(1)
                 .short(A_S_VARIABLE)
                 .long(A_L_VARIABLE)
-                .multiple_occurrences(true)
-                .required(false),
+                .value_hint(ValueHint::Other)
+                .value_name("KEY=VALUE")
+                .value_delimiter(',')
+                .action(ArgAction::Append),
         )
         .arg(
             Arg::new(A_L_VARIABLES_FILE)
@@ -64,32 +68,27 @@ pub fn args_matcher() -> App<'static> {
                     Empty lines, and those starting with \"#\" or \"//\" are ignored. \
                     See -D,--variable for specifying one pair at a time.",
                 )
-                .takes_value(true)
-                .forbid_empty_values(true)
+                .num_args(1)
                 .value_name("FILE")
                 .value_hint(ValueHint::FilePath)
+                // .value_parser(value_parser!(PathBuf))
                 .short(A_S_VARIABLES_FILE)
                 .long(A_L_VARIABLES_FILE)
-                .multiple_occurrences(true)
-                .required(false),
+                .action(ArgAction::Append),
         )
         .arg(
             Arg::new(A_L_ENVIRONMENT)
-                .help("use environment variables for substitution in the text")
-                .takes_value(false)
+                .help("Use environment variables for substitution in the text")
                 .short(A_S_ENVIRONMENT)
                 .long(A_L_ENVIRONMENT)
-                .multiple_occurrences(false)
-                .required(false),
+                .action(ArgAction::SetTrue),
         )
         .arg(
             Arg::new(A_L_VERBOSE)
                 .help("more verbose output (useful for debugging)")
-                .takes_value(false)
                 .short(A_S_VERBOSE)
                 .long(A_L_VERBOSE)
-                .multiple_occurrences(false)
-                .required(false),
+                .action(ArgAction::SetTrue),
         )
         .arg(
             Arg::new(A_L_LIST)
@@ -102,19 +101,15 @@ pub fn args_matcher() -> App<'static> {
                     and as many time as they appear in the input; \
                     i.e. there will be duplicates.",
                 )
-                .takes_value(false)
+                .action(ArgAction::SetTrue)
                 .short(A_S_LIST)
-                .long(A_L_LIST)
-                .multiple_occurrences(false)
-                .required(false),
+                .long(A_L_LIST),
         )
         .arg(
             Arg::new(A_L_FAIL_ON_MISSING_VALUES)
                 .help("fail if no value is available for a variable key found in the input text")
-                .takes_value(false)
+                .action(ArgAction::SetTrue)
                 .short(A_S_FAIL_ON_MISSING_VALUES)
-                .long(A_L_FAIL_ON_MISSING_VALUES)
-                .multiple_occurrences(false)
-                .required(false),
+                .long(A_L_FAIL_ON_MISSING_VALUES),
         )
 }
--- src/main.rs
+++ src/main.rs
@@ -13,37 +13,37 @@ use std::collections::HashMap;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = cli::args_matcher().get_matches();
 
-    let verbose = args.is_present(cli::A_L_VERBOSE);
-    let list = args.is_present(cli::A_L_LIST);
-    let src = args.value_of(cli::A_L_INPUT);
-    let dst = args.value_of(cli::A_L_OUTPUT);
+    let verbose = args.get_flag(cli::A_L_VERBOSE);
+    let list = args.get_flag(cli::A_L_LIST);
+    let src = args.get_one::<String>(cli::A_L_INPUT).cloned();
+    let dst = args.get_one::<String>(cli::A_L_OUTPUT).cloned();
 
     if list {
-        let detected_vars = replacer::extract_from_file(src)?;
-        tools::write_to_file(detected_vars, dst)?;
+        let detected_vars = replacer::extract_from_file(src.as_deref())?;
+        tools::write_to_file(detected_vars, dst.as_deref())?;
     } else {
         let mut vars = HashMap::new();
 
         // enlist environment variables
-        if args.is_present(cli::A_L_ENVIRONMENT) {
+        if args.get_flag(cli::A_L_ENVIRONMENT) {
             tools::append_env(&mut vars);
         }
         // enlist variables from files
-        if let Some(var_files) = args.values_of(cli::A_L_VARIABLES_FILE) {
+        if let Some(var_files) = args.get_many::<String>(cli::A_L_VARIABLES_FILE) {
             for var_file in var_files {
                 let mut reader = tools::create_input_reader(Some(var_file))?;
                 vars.extend(key_value::parse_vars_file_reader(&mut reader)?);
             }
         }
         // enlist variables provided on the CLI
-        if let Some(variables) = args.values_of(cli::A_L_VARIABLE) {
+        if let Some(variables) = args.get_many::<String>(cli::A_L_VARIABLE) {
             for key_value in variables {
                 let pair = key_value::Pair::parse(key_value)?;
                 vars.insert(pair.key.to_owned(), pair.value.to_owned());
             }
         }
 
-        let fail_on_missing: bool = args.is_present(cli::A_L_FAIL_ON_MISSING_VALUES);
+        let fail_on_missing = args.get_flag(cli::A_L_FAIL_ON_MISSING_VALUES);
 
         let settings = settings! {
             vars: vars,
@@ -51,7 +51,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             verbose: verbose
         };
 
-        replacer::replace_in_file(src, dst, &settings)?;
+        replacer::replace_in_file(src.as_deref(), dst.as_deref(), &settings)?;
     }
 
     Ok(())
